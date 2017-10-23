0adb4c040b53bd60a00b35d31104301b07e0f5c8
佐藤優大 <pikadon555@gmail.com>
2017-10-23 16:14:45 +0900

    boltレビュー第１回

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-
+jQuery(function($){
+      $('.humburgar').click(function(){
+         if ($('#button').css('display') == 'none') {
+          $('#button').slideDown('fast');
+        } else {
+           $('#button').slideUp('fast');
+         }
+      });
+        });
